a4b6f3681ba0485bca670f05519abe0d84fb3f70
Liyun Xiu <xiliyun@amazon.com>
2026-02-05 16:52:01 +0800

    add sve support
    Signed-off-by: Liyun Xiu <xiliyun@amazon.com>

--- amaiss/utils/distance_simd.h
+++ amaiss/utils/distance_simd.h
@@ -7,6 +7,8 @@
 #include "amaiss/utils/distance_avx512.h"
 #elif defined(__AVX2__)
 #include "amaiss/utils/distance_avx2.h"
+#elif defined(__ARM_FEATURE_SVE)
+#include "amaiss/utils/distance_sve.h"
 #elif defined(__aarch64__)
 #include "amaiss/utils/distance_neon.h"
 #else
